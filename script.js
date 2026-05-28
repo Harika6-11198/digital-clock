d1b20650ea6678bd431ec68d79b4c75613460822
Mittapalli Harika <harikamittapalli2020@gmail.com>
2026-05-28 11:29:00 +0530

    digital clock upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function updateClock(){
+
+    let now = new Date();
+
+    let hours = now.getHours();
+
+    let minutes = now.getMinutes();
+
+    let seconds = now.getSeconds();
+
+    hours = String(hours).padStart(2,'0');
+
+    minutes = String(minutes).padStart(2,'0');
+
+    seconds = String(seconds).padStart(2,'0');
+
+    document.getElementById("clock").innerText =
+    `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
